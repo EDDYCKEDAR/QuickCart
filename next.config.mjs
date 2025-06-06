1f6c6f34e34261109aed2352dad5e3d9e590ae49
EDDYCKEDAR <eddykedar@gmail.com>
2025-06-06 14:09:32 +0200

    update alias issues

--- next.config.mjs
+++ next.config.mjs
@@ -16,4 +16,15 @@ const nextConfig = {
     },
 };
 
+// next.config.js
+const path = require('path');
+
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
+
+
 export default nextConfig;
